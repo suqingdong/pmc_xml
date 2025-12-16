5bd978e8e273168078734ae814eb2a9d8e67df90
suqingdong <1078595229@qq.com>
2025-12-16 16:33:20 +0800

    fix body parser

--- pmc_xml/core/body.py
+++ pmc_xml/core/body.py
@@ -1,3 +1,6 @@
+import re
+
+
 def parse_body(body):
     if body is None:
         return []
@@ -7,6 +10,8 @@ def parse_body(body):
             title = sec.find('title').text
             sec = sec.find('sec') if sec.find('sec') is not None else sec
             text = '\n\n'.join(''.join(p.itertext()) for p in sec.findall('p'))
+            # remove unicode whitespace except \n
+            text = re.sub(r'[^\S\n]+', ' ', text).strip()
             yield {'title': title, 'text': text}
     body = list(get_data())
     return body
