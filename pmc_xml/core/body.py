644e7f2c073bcd9b49ede777c850808124020282
suqingdong <1078595229@qq.com>
2025-12-16 15:10:20 +0800

    add body parser

---
+++ pmc_xml/core/body.py
@@ -0,0 +1,9 @@
+def parse_body(body):
+    def get_data():
+        for sec in body.iterfind('sec'):
+            title = sec.find('title').text
+            sec = sec.find('sec') if sec.find('sec') is not None else sec
+            text = '\n\n'.join(''.join(p.itertext()) for p in sec.findall('p'))
+            yield {'title': title, 'text': text}
+    body = list(get_data())
+    return body
